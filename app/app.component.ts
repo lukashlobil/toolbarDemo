fe3b0b5bc079433223daf2564721ea3ce3c740ab
Lukas Hlobil <lhlobil@gmail.com>
2016-11-29 13:30:52 +0100

    routerOutlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,6 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "my-app",
-    template: `<StackLayout><sharedComponent></sharedComponent><page-router-outlet></page-router-outlet></StackLayout>`,
+    template: `<StackLayout><sharedComponent></sharedComponent><router-outlet></router-outlet></StackLayout>`,
 })
 export class AppComponent {}
